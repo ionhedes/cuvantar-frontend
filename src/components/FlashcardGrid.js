b5e64508ff09780447bacb03bdd16c256fd6997e
Ioan Hedeș <ihedes13@gmail.com>
2022-05-13 16:41:54 +0300

    added grey box contour to flashcard grid

--- src/components/FlashcardGrid.js
+++ src/components/FlashcardGrid.js
@@ -1,16 +1,32 @@
 import { Grid } from '@mui/material';
 import * as React from 'react';
 import Flashcard from "./Flashcard";
+import Box from "@mui/material/Box";
 
 class FlashcardGrid extends React.Component {
     render() {
-        return <Grid container spacing={4} justifyContent='center' alignContent='center'>
-            {this.props.cards.map((card) =>
-                <Grid item>
-                    <Flashcard word={card.get('word')} translation={card.get('translation')} />
+        return (
+            <Box
+                component="div"
+                sx={{display: "flex",
+                    minWidth: {xs: '80vw', md: '60vw'},
+                    maxWidth: '80vw',
+                    backgroundColor: '#e0e0e0',
+                    borderRadius: '10px',
+                    padding: '20px',
+
+                }}
+            >
+                <Grid container spacing={4} justifyContent='center' alignContent='center' minHeight="100">
+                    {this.props.cards.map((card) =>
+                        <Grid item>
+                            <Flashcard word={card.word} translation={card.translation} />
+                        </Grid>
+                    )}
                 </Grid>
-            )}
-        </Grid>;
+            </Box>
+
+        );
     }
 }
 
