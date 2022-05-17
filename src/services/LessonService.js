1de68e04c3c8404f8dce94b62e1b5b492de51f95
Ioan Hedeș <ihedes13@gmail.com>
2022-05-17 19:37:07 +0300

    now lessons from last session aren't cleared when submitted

--- src/services/LessonService.js
+++ src/services/LessonService.js
@@ -9,11 +9,13 @@ export function fetchLessonsFromServer() {
         mode: 'cors'
     };
 
+    sessionStorage.removeItem("lessons")
+
     return fetch(`http://localhost:3000/api/lessons?username=${encodeURIComponent(sessionStorage.getItem("username"))}`, requestOptions).then(
         res => res.json()
     ).then(
         data => {
-            sessionStorage.setItem('lessons', JSON.stringify(data));
+            sessionStorage.setItem("lessons", JSON.stringify(data));
             return data;
         }
     );
@@ -39,6 +41,4 @@ export function finishLessonSession() {
             encodeURIComponent(card.id)
         }`, requestOptions)
     });
-
-    sessionStorage.removeItem("lessons");
 }
